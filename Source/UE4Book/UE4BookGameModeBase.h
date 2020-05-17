c2bf7a4db19228d0748cbaff754188527384b2b2
Saulo Soares <saulosoares150498@gmail.com>
2020-05-17 12:10:21 -0300

    Initial commit

---
+++ Source/UE4Book/UE4BookGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4BookGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4BOOK_API AUE4BookGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
